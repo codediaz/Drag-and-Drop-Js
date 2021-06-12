0c2ff23fa9c9a559326c53af0fc63033ddf02c0b
Sergio Díaz <codediaz@outlook.es>
2021-06-11 22:33:50 -0500

    Draggable

---
+++ Main/main.js
@@ -0,0 +1,7 @@
+const box = document.querySelector('#box')
+box.addEventListener('dragstart', e => {
+    console.log('Drag Start')
+})
+box.addEventListener('dragend', e => {
+    console.log('Drag End')
+})
