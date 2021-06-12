2b5cfa83a38bcce33d8d11f02971698bfdafd3ef
Sergio Díaz <codediaz@outlook.es>
2021-06-11 22:51:40 -0500

    1st Drag an drop

--- Main/main.js
+++ Main/main.js
@@ -8,3 +8,23 @@ box.addEventListener('dragend', e => {
 box.addEventListener('dragend', e => {
     console.log('Drag')
 })
+
+
+
+//container 
+
+const container = document.querySelector('#container')
+container.addEventListener('dragenter', e =>{
+    console.log('Drag Enter')
+})
+container.addEventListener('dragleave', e =>{
+    console.log('Drag Leave')
+})
+container.addEventListener('dragover', e =>{
+    e.preventDefault();
+    console.log('Drag Over')
+})
+container.addEventListener('drop', e =>{
+    console.log('Drop')
+    container.appendChild(box);
+})
